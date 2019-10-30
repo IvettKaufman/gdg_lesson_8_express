688f5f749030234be5b0d32d9c4df3de0ab97590
Ivett Kaufman <ivett.kaufman@Ivetts-MacBook-Air.local>
2019-10-30 20:32:20 +0000

    add changes

--- public/main.js
+++ public/main.js
@@ -1,1 +1,3 @@
 /* this javascript file is referenced in the index.html and will be sent to the client. This code is not executed server-side */
+var message = alert('How would you name this little kitty?');
+document.write(message);
